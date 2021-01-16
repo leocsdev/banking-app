f1b331fd4e33e527e8681d33bc038c5b97c3c533
Leo <leocsdev@gmail.com>
2021-01-16 11:52:47 +0800

    Added deposit function.

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ let User = function (user, balance) {
 function createUser(user, balance = 0) {
   let newUser = new User(user, balance);
 
-  if (ifUserExist(user)) {
+  if (userExist(user)) {
     console.log("User already exists.");
   } else {
     users.push(newUser);
@@ -35,7 +35,7 @@ function createUser(user, balance = 0) {
 }
 
 // Check if user exists
-function ifUserExist(user) {
+function userExist(user) {
   if (users.length) {
     let i = 0;
 
@@ -55,18 +55,29 @@ function ifUserExist(user) {
   }
 }
 
-/*
-Here are the required functions.
+function deposit(user, amount) {
+  // Try to make the amount as accurate as possible (search for floating points)
+  // check if amount is a valid number, CANNOT BE NEGATIVE NUMBER
+  if (amount <= 0) {
+    console.log("Please enter valid amount.");
+  } else {
+    // Get user information from users array
+    if (userExist(user)) {
+      // Once user is found, add the amount to the balance AND RETURN new balance
 
-• create_user(user, balance)
-• function creates new user in the system
-• New user has zero balance (or an optional initial balance)
-• user (argument) is any string value
-• deposit(user, amount)
-• increases user's balance by amount value
-• returns new_balance of the user
-• withdraw(user, amount)
-• decreases user's balance by amount value
-• returns new_balance of the user
-
-*/
+      for (let i = 0; i < users.length; i++) {
+        // console.log(users[i].user);
+        if (users[i].user == user) {
+          // console.log(users[i].user);
+          users[i].balance += amount;
+          console.log(
+            `New balance for ${users[i].user} is ${users[i].balance}`
+          );
+        }
+      }
+    } else {
+      // If user not found, show message -> 'User does not exist.'
+      console.log("User does not exist.");
+    }
+  }
+}
