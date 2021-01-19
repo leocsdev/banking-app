47d54cd367733dca8a5452a14dfcc522ab7b6374
Leo <leocsdev@gmail.com>
2021-01-20 00:33:54 +0800

    Removed commented function.

--- js/app.js
+++ js/app.js
@@ -130,41 +130,6 @@ function withdraw(user, amount = 0) {
   }
 }
 
-// function withdraw(user, amount = 0) {
-//   // Try to make the amount as accurate as possible (search for floating points)
-//   // check if amount is a valid number, CANNOT BE NEGATIVE NUMBER
-//   if (amount <= 0) {
-//     let error = "Please enter valid amount.";
-//     return error;
-//   }
-
-//   // Get user information from users array
-//   if (userExist(user)) {
-//     // Once user is found, add the amount to the balance AND RETURN new balance
-//     for (let i = 0; i < users.length; i++) {
-//       // console.log(users[i].user);
-//       if (users[i].user.toUpperCase() == user.toUpperCase()) {
-//         // check balance if enough to make a withdrawal
-//         // console.log(users.toUpperCase().balance);
-//         if (users[i].balance < amount) {
-//           // console.log("Sorry, balance insufficient");
-//           return "Sorry, balance insufficient";
-//         } else {
-//           users[i].balance -= amount;
-//           console.log(
-//             `New balance for ${users[i].user} is ${users[i].balance}`
-//           );
-//           return users[i].balance;
-//         }
-//       }
-//     }
-//   } else {
-//     // If user not found, show message -> 'User does not exist.'
-//     // console.log("User does not exist.");
-//     return "User does not exist.";
-//   }
-// }
-
 function send(from, to, amount) {
   // check both users if existing
   if (userExist(from) && userExist(to) && amount > 0) {
